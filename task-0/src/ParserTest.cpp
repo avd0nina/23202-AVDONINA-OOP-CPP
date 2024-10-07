c82d11754cca828207ef44bf39228e85971241da
avd0nina <144753760+avd0nina@users.noreply.github.com>
2024-10-07 15:59:06 +0700

    Update ParserTest.cpp

--- task-0/src/ParserTest.cpp
+++ task-0/src/ParserTest.cpp
@@ -1,15 +1,35 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include <string>
+#include <gtest/gtest.h>
+#include "Parser.h"
 #include <list>
+#include <string>
 
 using namespace std;
 
-class Parser {
-public:
-    list<string> splitIntoWords(const string& line);
-};
+TEST(Parser, SplitIntoWords_Basic) {
+Parser parser;
+string line = "Hello, world!";
+list<string> words = parser.splitIntoWords(line);
 
-#endif // PARSER_H
+list<string> expected = {"hello", "world"};
+EXPECT_EQ(words, expected);
+}
+
+TEST(Parser, SplitIntoWords_Empty) {
+Parser parser;
+string line = "";
+list<string> words = parser.splitIntoWords(line);
+
+EXPECT_TRUE(words.empty());
+}
+
+TEST(Parser, SplitIntoWords_SpecialChars) {
+Parser parser;
+string line = "C++ is great!";
+list<string> words = parser.splitIntoWords(line);
+
+list<string> expected = {"c", "is", "great"};
+EXPECT_EQ(words, expected);
+}
+
+
 
