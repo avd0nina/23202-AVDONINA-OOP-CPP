fc56739ad6862ebcace698e496518d17a38951eb
avdonina lada <144753760+avd0nina@users.noreply.github.com>
2024-10-28 08:24:48 +0700

    Update BitArray_test.cpp

--- task-1/tests/BitArray_test.cpp
+++ task-1/tests/BitArray_test.cpp
@@ -1,14 +1,12 @@
 #include <gtest/gtest.h>
 #include "../src/BitArray.h"
 
-// Тесты конструктора по умолчанию
 TEST(BitArrayTest, DefaultConstructor) {
 BitArray ba;
 EXPECT_EQ(ba.size(), 0);
 EXPECT_TRUE(ba.empty());
 }
 
-// Тесты конструктора с параметрами
 TEST(BitArrayTest, ParameterizedConstructor) {
 BitArray ba(10, 5); // 10 битов, первые биты - 0b101
 EXPECT_EQ(ba.size(), 10);
@@ -21,7 +19,6 @@ EXPECT_EQ(ba[i], false);
 }
 }
 
-// Тесты метода set и оператора []
 TEST(BitArrayTest, SetAndAccess) {
 BitArray ba(5);
 ba.set(2);
@@ -34,7 +31,6 @@ EXPECT_THROW(ba.set(-1), std::out_of_range);
 EXPECT_THROW(ba.set(5), std::out_of_range);
 }
 
-// Тесты метода reset
 TEST(BitArrayTest, Reset) {
 BitArray ba(5);
 ba.set();
@@ -46,7 +42,6 @@ EXPECT_EQ(ba[i], false);
 }
 }
 
-// Тесты методов any, none, count
 TEST(BitArrayTest, AnyNoneCount) {
 BitArray ba(5);
 EXPECT_FALSE(ba.any());
@@ -60,14 +55,12 @@ ba.set(2);
 EXPECT_EQ(ba.count(), 2);
 }
 
-// Тесты оператора ~
 TEST(BitArrayTest, BitwiseNot) {
 BitArray ba(5, 0b10101);
 BitArray ba_not = ~ba;
 EXPECT_EQ(ba_not.to_string(), "01010");
 }
 
-// Тесты операторов сравнения
 TEST(BitArrayTest, ComparisonOperators) {
 BitArray ba1(5, 0b10101);
 BitArray ba2(5, 0b10101);
@@ -79,13 +72,11 @@ EXPECT_FALSE(ba1 == ba3);
 EXPECT_FALSE(ba1 == BitArray(4, 0b1010));
 }
 
-// Тесты метода to_string
 TEST(BitArrayTest, ToString) {
 BitArray ba(8, 0b11001100);
 EXPECT_EQ(ba.to_string(), "00110011");
 }
 
-// Тесты метода swap
 TEST(BitArrayTest, Swap) {
 BitArray ba1(5, 0b10101);
 BitArray ba2(5, 0b01010);
@@ -94,7 +85,6 @@ EXPECT_EQ(ba1.to_string(), "01010");
 EXPECT_EQ(ba2.to_string(), "10101");
 }
 
-// Тесты метода clear
 TEST(BitArrayTest, Clear) {
 BitArray ba(5, 0b10101);
 ba.clear();
@@ -102,7 +92,6 @@ EXPECT_EQ(ba.size(), 0);
 EXPECT_TRUE(ba.empty());
 }
 
-// Тесты исключений при неверных индексах
 TEST(BitArrayTest, IndexOutOfBounds) {
 BitArray ba(5);
 EXPECT_THROW(ba.set(-1), std::out_of_range);
@@ -110,4 +99,3 @@ EXPECT_THROW(ba.set(5), std::out_of_range);
 EXPECT_THROW(ba[-1], std::out_of_range);
 EXPECT_THROW(ba[5], std::out_of_range);
 }
-
