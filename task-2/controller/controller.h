62227c7cb3588238e7970ddb9ebdf158026b8338
avdonina lada <144753760+avd0nina@users.noreply.github.com>
2024-12-06 17:10:02 +0700

    Create controller.h

---
+++ task-2/controller/controller.h
@@ -0,0 +1,27 @@
+#ifndef TASK_2_CONTROLLER_H
+#define TASK_2_CONTROLLER_H
+
+#pragma once
+#include "../model/model.h"
+#include "../view/view.h"
+#include <memory>
+
+class SimulationController {
+private:
+    std::unique_ptr<Game_model> model_;
+    Game_view view_;
+    std::string out_file;
+
+public:
+    SimulationController();
+    SimulationController(const std::string& infile);
+    SimulationController(
+            const std::string& infile,
+            const std::string& ticks,
+            const std::string& outfile
+    );
+
+    void play_game();
+};
+
+#endif //TASK_2_CONTROLLER_H
